c8a6c0183232bd382aab060c571cde21eb139055
Eitaro Fukamachi <e.arrows@gmail.com>
2019-03-20 15:01:29 +0900

    Cache the parsed configuration files.

--- utils/config.lisp
+++ utils/config.lisp
@@ -5,28 +5,43 @@
   (:import-from #:parser.ini
                 #:*include-empty-sections?*
                 #:parse)
-  (:export #:read-from-file
+  (:export #:parse-file
+           #:read-from-file
            #:*aws-profile*))
 (in-package #:aws-sdk/utils/config)
 
 (defvar *aws-profile* (or (getenv "AWS_PROFILE") "default"))
 
-;; TODO: Cache the parsed data
-(defun read-from-file (file &key (profile *aws-profile*))
+(defvar *cached-ini* (make-hash-table :test 'equal))
+
+(defun %parse-file (file)
   (let* ((parser.ini:*include-empty-sections?* t)
-         (data (parser.ini:parse file 'list))
-         (section (or (find-if (lambda (section)
-                                 (string= (first (getf section :name)) profile))
-                               data)
-                      ;; Fallback to 'profile <name>'
-                      (find-if (lambda (section)
-                                 (string= (first (getf section :name)) (format nil "profile ~A" profile)))
-                               data))))
+         (data (parser.ini:parse file 'list)))
+    (mapcar (lambda (section)
+              (cons (first (getf section :name))
+                    (mapcar (lambda (option)
+                              (cons (first (getf (first option) :name))
+                                    (getf (first option) :value)))
+                            (getf (getf section :section) :section-option))))
+            data)))
+
+(defun parse-file (file)
+  (let ((filename (namestring file)))
+    (when (and (gethash filename *cached-ini*)
+               (<= (file-write-date filename)
+                   (car (gethash filename *cached-ini*))))
+      (return-from parse-file (cdr (gethash filename *cached-ini*))))
+    (let ((data (%parse-file file)))
+      (setf (gethash filename *cached-ini*)
+            (cons (file-write-date file) data))
+      data)))
+
+(defun read-from-file (file &key (profile *aws-profile*))
+  (let* ((data (parse-file file))
+         (section
+           (or (assoc profile data :test 'equal)
+               ;; Fallback to 'profile <name>'
+               (assoc (format nil "profile ~A" profile) data :test 'equal))))
     (unless section
       (error "Profile '~A' doesn't exist in '~A'." profile file))
-
-    (mapcar (lambda (option)
-              (cons
-               (first (getf (first option) :name))
-               (getf (first option) :value)))
-            (getf (getf section :section) :section-option))))
+    (cdr section)))
