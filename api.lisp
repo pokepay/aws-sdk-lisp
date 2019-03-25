f9736c406ad47955a71fa1ba7574bdbfa2d0da86
Eitaro Fukamachi <e.arrows@gmail.com>
2019-03-25 17:09:10 +0900

    Sign X-Amz-Security-Token header.

--- api.lisp
+++ api.lisp
@@ -27,6 +27,9 @@
       (error "AWS region is not configured"))
     (let ((host (aws-host service region))
           (aws-sign4:*aws-credentials* (lambda () (credentials-keys credentials))))
+      (setf headers
+            (append (credentials-headers credentials)
+                    headers))
       (multiple-value-bind (authorization x-amz-date)
           (aws-sign4:aws-sign4 :region region
                                :service service
@@ -42,7 +45,6 @@
                      :headers `(("Authorization" . ,authorization)
                                 ("X-Amz-Date" . ,x-amz-date)
                                 ("X-Amz-Content-Sha256" . ,(aws-sdk/utils::sha-256 (or payload "")))
-                                ,@(credentials-headers credentials)
                                 ("Content-Type" . "application/x-amz-json-1.0")
                                 ,@headers)
                      :content payload)))))
