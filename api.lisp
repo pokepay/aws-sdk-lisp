a5eee87ac9a10b3ebea4acbb50d73317b703fc5e
Eitaro Fukamachi <e.arrows@gmail.com>
2022-12-15 02:02:21 +0000

    Make each requests to be an object to allow to customize.

--- api.lisp
+++ api.lisp
@@ -8,6 +8,17 @@
                 #:credentials-keys
                 #:credentials-headers
                 #:default-aws-credentials)
+  (:import-from #:aws-sdk/request
+                #:request
+                #:request-session
+                #:request-service
+                #:request-headers
+                #:request-payload
+                #:request-method
+                #:request-path
+                #:request-params
+                #:request-host
+                #:request-endpoint)
   (:import-from #:aws-sign4)
   (:import-from #:dexador)
   (:import-from #:quri)
@@ -15,35 +26,32 @@
            #:aws-request))
 (in-package #:aws-sdk/api)
 
-(defun aws-host (service region)
-  (format nil "~(~A~).~(~A~).amazonaws.com" service region))
-
-(defun aws-request (&key (path "/") service method params headers payload
-                      (session *session*))
-  (let ((credentials (or (session-credentials session)
-                         (default-aws-credentials)))
-        (region (session-region session)))
+(defun aws-request (req)
+  (check-type req request)
+  (let* ((session (request-session req))
+         (credentials (or (session-credentials session)
+                          (default-aws-credentials)))
+         (region (session-region session))
+         (service (request-service req)))
     (unless credentials
       (error "No credentials are found"))
     (unless region
       (error "AWS region is not configured"))
-    (let ((host (aws-host service region))
-          (aws-sign4:*aws-credentials* (lambda () (credentials-keys credentials))))
-      (setf headers
-            (append (credentials-headers credentials)
-                    headers))
+    (let ((aws-sign4:*aws-credentials* (lambda () (credentials-keys credentials)))
+          (headers (append (credentials-headers credentials)
+                           (request-headers req)))
+          (payload (request-payload req)))
       (multiple-value-bind (authorization x-amz-date)
           (aws-sign4:aws-sign4 :region region
                                :service service
-                               :method method
-                               :host host
-                               :path path
-                               :params params
+                               :method (request-method req)
+                               :host (request-host req region)
+                               :path (request-path req)
+                               :params (request-params req)
                                :headers headers
                                :payload (or payload ""))
-        (dex:request (format nil "https://~A~A?~A" host path
-                             (quri:url-encode-params params))
-                     :method method
+        (dex:request (request-endpoint req region)
+                     :method (request-method req)
                      :headers `(("Authorization" . ,authorization)
                                 ("X-Amz-Date" . ,x-amz-date)
                                 ("X-Amz-Content-Sha256" . ,(aws-sdk/utils::sha-256 (or payload "")))
